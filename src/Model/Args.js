117f5593e49dc086668a426f0d739246e286adc1
Adam Saladino <adam.saladino@ellucian.com>
2018-01-11 16:14:06 -0500

    Doing some cleanup.

--- src/Model/Args.js
+++ src/Model/Args.js
@@ -1,22 +1,40 @@
 const FileDetails = require('./FileDetails');
 
+/**
+ * Commandline arguments being passed to the app.
+ */
 class Args {
     constructor(params) {
         /**
-         * @type FileDetails
+         * Project directory to output the app results.
+         * @type {FileDetails|null}
          */
         this.output = null;
         /**
-         * @type {string}
+         * Domain being indexed.
+         * @type {string|*}
          */
         this.domain = null;
+        /**
+         * Should progress information be output to the console?
+         * @type {boolean}
+         */
+        this.verbose = true;
         Object.assign(this, params);
     }
 
+    /**
+     * If the mandatory options are not passed then show the menu.
+     * @returns {boolean} true if the mandatory options are not passed.
+     */
     shouldShowHelp() {
-        return this.hasOwnProperty('help') || !this.domain || !this.output;
+        return this.hasOwnProperty('help') || (this.domain === null || this.output === null);
     }
 
+    /**
+     * Get the site name from the domain.
+     * @returns {string} the site name.
+     */
     getSiteName() {
         return this.domain.replace(/[.]/g, '_');
     }
--- src/Service/LighthouseService.js
+++ src/Service/LighthouseService.js
@@ -4,7 +4,6 @@ const lighthouse = require('lighthouse');
 const Printer = require('lighthouse/lighthouse-cli/printer');
 const UrlsRepository = require('../Repository/UrlsRepository');
 const OptionsRepository = require('../Repository/OptionsRepository');
-const Progress = require('../Model/Progress');
 const Url = require('../Model/Url');
 const path = require("path");
 const fs = require("fs");
@@ -12,6 +11,7 @@ const fs = require("fs");
 class LighthouseService {
 
     constructor(args) {
+        this.events = new Map();
         this.args = args;
         this.optionsRepository = new OptionsRepository(this.args);
         this.option = this.optionsRepository.getOption();
@@ -19,22 +19,23 @@ class LighthouseService {
         this.createOutputFolder();
     }
 
-    async start(start = count => {}, update = progress => {}) {
+    async start() {
         ChromeLauncher.launch({
             chromeFlags: ["--headless"]
-        }).then(chrome => this.runReports(start, update, chrome));
+        }).then(chrome => this.runReports(chrome));
     }
 
-    async runReports(start = count => {}, update = progress => {}, chrome) {
+    async runReports(chrome) {
         let flags = {};
         flags.port = chrome.port;
         let urls = this.urlsRepository.findForRange();
-        start(urls.length);
+        this.emitStart(urls);
         for (let url of urls) {
             let results = await lighthouse(url.url, flags);
             await this.printReport(results, url);
-            update(new Progress(url, urls.length));
+            this.emitProgress(url);
         }
+        this.emitComplete();
         chrome.kill();
     }
 
@@ -55,6 +56,52 @@ class LighthouseService {
         if (!fs.existsSync(this.folder)) {
             fs.mkdirSync(this.folder)
         }
+    }
+
+    /**
+     * Receive event information.
+     * @param event {string} name of the event. (start, progress, and complete)
+     * @param callback {Function} called when the event is emitted.
+     * @returns {LighthouseService} for chaining.
+     */
+    on(event, callback) {
+        this.events.set(event, callback);
+        return this;
+    }
+
+    /**
+     * Emits that start event.
+     * @param urls {[Url]} found at start.
+     */
+    emitStart(urls) {
+        this.events.forEach((callback, event) => {
+            if (event === 'start') {
+                callback(urls);
+            }
+        });
+    }
+
+    /**
+     * Emits that progress event.
+     * @param url {Url} that is currently having its content extracted from.
+     */
+    emitProgress(url) {
+        this.events.forEach((callback, event) => {
+            if (event === 'progress') {
+                callback(url);
+            }
+        });
+    }
+
+    /**
+     * Emits that complete event when service has finished.
+     */
+    emitComplete() {
+        this.events.forEach((callback, event) => {
+            if (event === 'complete') {
+                callback();
+            }
+        });
     }
 }
 
